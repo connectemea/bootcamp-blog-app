e2147ddd5dcedc12288325b9c6edac489b891a30
aseelkp <aseelaroor10@gmail.com>
2021-10-26 23:01:44 +0530

    card

--- src/components/utils/Blog.js
+++ src/components/utils/Blog.js
@@ -1,7 +1,8 @@
+import styles from './Blog.css'
 export default function Blog(props) {
   const { title, desc } = props;
   return (
-    <div>
+    <div className={styles.Card}>
       <h3>{title}</h3>
       <p>{desc}</p>
     </div>
