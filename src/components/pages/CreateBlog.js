41b912f2bf1589c9cc9e6049c3daeb9c7224e330
aseelkp <aseelaroor10@gmail.com>
2021-10-28 09:25:55 +0530

    completed

--- src/components/pages/CreateBlog.js
+++ src/components/pages/CreateBlog.js
@@ -25,12 +25,12 @@ export default function CreateBlog (){
   return (
     <div className={styles.contentWrapper}>
       <div className={styles.cHeader}>
-      <Link to="home">back</Link><h3 className={styles.new}>New Blog</h3> .
+      <Link to="home" className={styles.backBtn} ><i class="fas fa-arrow-left"></i></Link><h3 className={styles.new}>New Blog</h3> .
       </div>
-      <div>
-        <input type="text" name="name" value={blog?.name} onChange={handleBlogChange}/>
-        <input type="text" name="desc" value={blog?.desc} onChange={handleBlogChange}/>
-        <button onClick={handleAddBlog}>Create</button>
+      <div className={styles.cFooter}>
+        <input placeholder="Add Title" type="text" name="name" value={blog?.name} className={styles.Title} onChange={handleBlogChange}/>
+        <input  type="text" name="desc" value={blog?.desc} className={styles.inp} onChange={handleBlogChange}/>
+        <button className={styles.Btn} onClick={handleAddBlog}>Publish</button>
       </div>
     </div>
   );
